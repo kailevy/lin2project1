7d3ab043d218946de0ac37b1ffed4e039acfa120
agreiner29 <aarongreiner@aarons-mbp.olin.edu>
2015-10-19 15:12:58 -0400

    added path tracing

--- bestroute.m
+++ bestroute.m
@@ -13,10 +13,10 @@ function res = bestroute(time, impact, cost)
         
     D  = [5 , 3, 2, 4 , 2 , 3 , 4 , 7 , 1, 3 , 8 , 4 , 2 , 5 , 4 , 10, 10, 7 , 8 , 8 , 4 , 3 , 4 , 4 , 4 ];
     Btpm = [4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4];
-    Bcpm = [.01,.01,.01,.01,.01,.01,.01,.01,.01,.01,.01,.01,.01,.01,.01,.01,.01,.01,.01,.01,.01,.01,.01,.01,.01];
+    Bcpm = [.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1,.1];
     Bipm = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
     
-    Ctpm = [0.45,0.43,0.22,0.21,0.2,0.3,0.33,0.44,0.25,0.3,0.36,0.21,0.33,0.63,0.33,0.71,0.67,0.88,0.47,0.44,0.24,0.6,0.21,0.36,1];
+    Ctpm =[2.2,2.3,4.5,4.75,5,3.3,3,2.3,4,3.3,2.75,4.75,3,1.6,3,1.4,1.5,1.1,2.13,2.25,4.25,1.7,4.75,2.75,1];
     Ccpm= [.2,.2,.2,.2 ,.2 , .2, .2, .2,.3,.2 ,.2 ,.2 ,.2 ,.2 ,.2 , .15, .1,.15 ,.2 ,.2 ,.2 ,.2 ,.2 ,.2 ,.2 ];
     Cipm= [.35,.45,.35,.35 ,.35 , .45, .25, .35,.45,.35 ,.35 ,.35 ,.35 ,.35 ,.35 , .25, .25,.25 ,.35 ,.35 ,.35 ,.35 ,.35 ,.35 ,.35 ];
     
@@ -29,37 +29,43 @@ function res = bestroute(time, impact, cost)
     WBc = D.*Bcpm;
     WBi = D.*Bipm;
     
-    WCt_norm = WCt ./ sum(WCt + WBt);
+    WCt_norm = WCt ./ sum(WCt + WBt)
     WCc_norm = WCc ./ sum(WCc + WBc);
     WCi_norm = WCi ./ sum(WCi + WBi);
     
     
-    WBt_norm = WBt ./ sum(WCt + WBt);
+    WBt_norm = WBt ./ sum(WCt + WBt)
     WBc_norm = WBc ./ sum(WCc + WBc);
     WBi_norm = WBi ./ sum(WCi + WBi);
     
     a  = [1 , 3, 3, 4 , 7 , 5 , 1 , 8 , 6, 7 , 7 , 13, 13, 11, 11, 1 , 10, 16, 14, 9 , 15, 12, 17, 18, 15];
     b  = [3 , 4, 5, 7 , 6 , 6 , 8 , 6 , 9, 9 , 13, 12, 18, 9 , 14, 10, 16, 14, 17, 15, 12, 2 , 2 , 2 , 17];
     
+    x  = [-2, 3, -1,  0, -.5 ,  0,  1, -1,  0, -2, 0, 2, 2, -.5, 1, -2 , 1, 3];
+    y  = [-1, 2, -2, -2, -1.5, -1, -1, -1,  0,  0, 1, 1, 0, 1.5, 1, 1.5, 2, 1];
     
     CW = weighted_values(time, impact, cost, 'car');
-    G = graph(a,b,CW);
+    CG = graph(a,b,CW);
     
-%     plot(G, 'EdgeLabel', G.Edges.Weight)
-    [Cpath,Cd] = shortestpath(G,1,2);
+%     plot(CG, 'EdgeLabel', CG.Edges.Weight)
+    [Cpath,Cd] = shortestpath(CG,1,2);
     
     BW = weighted_values(time, impact, cost, 'bike');
-    G = graph(a,b,BW);
+    BG = graph(a,b,BW);
     
-%     plot(G, 'EdgeLabel', G.Edges.Weight)
-    [Bpath,Bd] = shortestpath(G,1,2);
+%     plot(BG, 'EdgeLabel', BG.Edges.Weight)
+    [Bpath,Bd] = shortestpath(BG,1,2);
     
     if (Cd < Bd)
         disp('Drive');
         disp(Cpath);
+        p=plot(CG,'XData',x,'YData',y);
+        highlight(p,Cpath,'EdgeColor','r','linewidth',2);
     else
        disp('Bike');
        disp(Bpath);
+       p=plot(BG,'XData',x,'YData',y);
+       highlight(p,Bpath,'EdgeColor','r','linewidth',2);
     end
 
 end
