85458aefbc8acd5938b30c57fe95533295c98098
kailevy <kai0levy@gmail.com>
2015-10-21 11:43:23 -0400

    Comment calcweight and finish bestroute

--- calcweight.py
+++ calcweight.py
@@ -15,30 +15,42 @@ def find_n(l, val, n=1):
     return -1
 
 def find_two_list(l1, l2, v1, v2):
+    """
+    Finds two matching values in two lists
+    """
     found = False
     switched = False
     n = 1
-    lists = {'a': l1, 'b': l2}
+    lists = {1: l1, 2: l2}
     while not found:
-        idx_node = find_n(lists['a'], v1, n)
+        # finds the index of the nth occurence of the value in the first list
+        idx_node = find_n(lists[1], v1, n)
         if idx_node == -1 and not switched:
-            # print 'check other one'
-            lists = {'a': l2, 'b': l1}
-            n = 0
+            # can't find it in first list; switch and reset N
+            lists = {1: l2, 2: l1}
+            n = 1
             switched = True
         elif idx_node == -1 and switched:
-            print 'something is wrong'
+            # can't find value in either list..
+            # print 'something is wrong'
             return -1
-        if lists['b'][idx_node] == v2:
+        if lists[2][idx_node] == v2:
+            # the index of the first value in the first list matches the second
+            # value in the second list
             return idx_node
         else:
+            # increment n so we can check the next value
             n += 1
 
 def calc_weight(a, b, path, W):
+    """
+    Calculated the weight of a given path, and with two lists representing the graph a and b
+    where the weight between nodes are given by W
+    """
     weight = 0
     for node, node_next in zip(path, path[1:]):
         idx_node = find_two_list(a, b, node, node_next)
-        print 'time between' , node , 'and' , node_next , 'is' , W[idx_node]
+        # print 'time between' , node , 'and' , node_next , 'is' , W[idx_node]
         weight += W[idx_node]
     return weight
 
